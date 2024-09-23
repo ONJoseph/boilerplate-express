b4370de952371caaee2d80506cea94b141e407c1
Joseph Ogbole <on.josephinfo@gmail.com>
2024-09-23 20:38:35 +0100

    Serve index.html for root path using res.sendFile

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-// Serve the string "Hello Express" at the root path "/"
+// Serve the index.html file at the root path "/"
 app.get('/', function(req, res) {
-  res.send("Hello Express");
+  var absolutePath = path.join(__dirname, 'views', 'index.html');
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
