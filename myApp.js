776f5b3e62d2c37d1f6b4a2a73ca8fbf30ee8380
Joseph Ogbole <on.josephinfo@gmail.com>
2024-09-23 20:50:58 +0100

    Serve static assets from /public directory

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// Serve static files from the "public" directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 // Serve the index.html file at the root path "/"
 app.get('/', function(req, res) {
   var absolutePath = path.join(__dirname, 'views', 'index.html');
