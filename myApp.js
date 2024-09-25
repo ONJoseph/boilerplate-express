96a2c0d0b620bf15da4376cddaf88a6bfbc7361e
Joseph Ogbole <on.josephinfo@gmail.com>
2024-09-25 21:58:34 +0100

    Add MESSAGE_STYLE environment variable handling in /json route

--- myApp.js
+++ myApp.js
@@ -1,21 +1,26 @@
 var express = require('express');
-var app = express();
 var path = require('path');
+require('dotenv').config(); // Load environment variables
+
+var app = express();
 
 // Serve static files from the "public" directory
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Serve the index.html file at the root path "/"
 app.get('/', function(req, res) {
-  var absolutePath = path.join(__dirname, 'views', 'index.html');
-  res.sendFile(absolutePath);
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 // Serve JSON at the "/json" route
 app.get('/json', function(req, res) {
-    res.json({
-      message: "Hello json"
-    });
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({
+    message: message
   });
+});
 
 module.exports = app;
